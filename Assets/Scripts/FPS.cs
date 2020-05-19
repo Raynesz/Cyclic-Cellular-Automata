d4292d532b702974f406f8127d05a765ab04959c
Raynesz <raynes.reloaded@gmail.com>
2020-05-20 00:14:27 +0300

    tried some builds out

--- Assets/Scripts/FPS.cs
+++ Assets/Scripts/FPS.cs
@@ -5,18 +5,15 @@ using UnityEngine;
 public class FPS : MonoBehaviour
 {
     private float updateRateSeconds = 4.0F;
-    private int frameCount = 0;
     private float dt = 0.0F;
     public float fps = 0.0F;
 
     void Update()
     {
-        frameCount++;
         dt += Time.unscaledDeltaTime;
         if (dt > 1.0 / updateRateSeconds)
         {
             fps = 1.0f / Time.unscaledDeltaTime;
-            frameCount = 0;
             dt -= 1.0F / updateRateSeconds;
         }
     }
